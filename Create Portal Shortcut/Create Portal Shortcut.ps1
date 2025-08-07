8959c563f6b4695fae7fb831c2c07d618e47d2a9
Get-LocalUser <121067700+Get-LocalUser@users.noreply.github.com>
2025-08-06 19:49:28 -0700

    ...

--- Create Portal Shortcut/Create Portal Shortcut.ps1
+++ Create Portal Shortcut/Create Portal Shortcut.ps1
@@ -7,7 +7,6 @@ $companyPortalPath = Get-ChildItem -ErrorAction SilentlyContinue -Path "C:\Progr
 # Find the executable
 $companyPortalExe = Get-ChildItem -Path $companyPortalPath -Recurse | Where-Object { $_.Name -eq "CompanyPortal.exe" } | Select-Object -ExpandProperty FullName -First 1
 
-# Verify the executable was found
 if (-not $companyPortalExe) {
     Write-Output "CompanyPortal.exe not found. Exiting."
     Exit
@@ -18,25 +17,20 @@ $users = Get-ChildItem -Path "C:\Users" -Directory
 $startupFolderRelPath = "AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 
 foreach ($user in $users) {
-    # Construct the user's Startup folder path
     $startupPath = Join-Path -Path $user.FullName -ChildPath $startupFolderRelPath
 
-    # Check if the Startup folder exists
     if (-not (Test-Path -Path $startupPath)) {
         Write-Output "Startup folder not found for user $($user.Name). Skipping..."
         Continue
     }
 
-    # Define the shortcut path
     $shortcutPath = Join-Path -Path $startupPath -ChildPath "CompanyPortal.lnk"
 
-    # Check if the shortcut already exists
     if (Test-Path -Path $shortcutPath) {
         Write-Output "Shortcut already exists for user $($user.Name). Skipping..."
         Continue
     }
 
-    # Create the shortcut
     $wshShell = New-Object -ComObject WScript.Shell
     $shortcut = $wshShell.CreateShortcut($shortcutPath)
     $shortcut.TargetPath = $companyPortalExe
--- Launch Portal/Detection.ps1
+++ Launch Portal/Detection.ps1
@@ -1,19 +1,15 @@
-# Constants
 $markerFilePath = "C:\ProgramData\MarkerFiles\CompanyPortalLaunchMarker.txt"
 
-# Ensure the ProgramData\MarkerFiles directory exists
 $markerDirectory = Split-Path -Path $markerFilePath -Parent
 if (-not (Test-Path -Path $markerDirectory)) {
     New-Item -Path $markerDirectory -ItemType Directory -Force | Out-Null
 }
 
-# Check if the marker file already exists
 if (Test-Path -Path $markerFilePath) {
     Write-Output "Launch marker found. Reporting as already launched."
-    exit 0  # Detection confirmed
+    exit 0 
 }
 
-# Check if the shortcut exists
 if (-not(Test-Path -Path $markerFilePath)) {
     Write-Output "Marker file not detected at $markerFilePath"
     exit 1  # Detection failed
